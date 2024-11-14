5763de7ecf8c1bb7328117f3cd53e34b357cc6b2
Yoshio Kinoshita <yoshio.kinoshita@grape-eng.com>
2024-11-14 18:09:47 +0900

    u js yml

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,6 +1,6 @@
 const env = process.env.NODE_ENV || "development";
 
-if (env === "development" || env === "test") {
+if (env === "production" || env === "development" || env === "test") {
   const config = require("./config.json");
   const envConfig = config[env];
   console.log(envConfig);
