8eb72d5e4fa2c8656b669cc978adcbf0c076afdb
Yoshio Kinoshita <yoshio.kinoshita@grape-eng.com>
2024-11-16 10:56:01 +0900

    modified env files

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -5,4 +5,6 @@ const apiClient = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
 
+console.log(`process.env.REACT_APP_API_URL: ${process.env.REACT_APP_API_URL}`);
+
 export default apiClient;
